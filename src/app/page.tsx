54e4c59e2a73822c670d125f987b798d1f6c0a48
Marius <marius.sergent@gmail.com>
2025-05-15 16:35:40 +0200

    add refresh on add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ProjectsList from '@/components/lists/projects-list'
 import Project from '@/db/models/project'
 import { connect } from '@/lib/db'
 import { IProject } from '@/types/interfaces'
+import { revalidatePath } from 'next/cache'
 
 export default async function Home (): Promise<React.ReactNode> {
   await connect()
@@ -12,11 +13,12 @@ export default async function Home (): Promise<React.ReactNode> {
     'use server'
     const _project = new Project(project)
     await _project.save()
+    revalidatePath('/')
   }
 
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+      <main className='flex flex-col gap-[32px] row-start-2 items-center'>
         <CreateProjectDialog createProject={createProject} />
         <ProjectsList projects={projects} />
       </main>
